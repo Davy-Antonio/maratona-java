fa919cabe3fa5e47767d0b890f472de096ea25df
Davy Antonio <davyborges5@gmail.com>
2026-08-17 23:22:52 -0300

    Arrays

---
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays01 {
+    static void main() {
+        int [] idades = new int[3];
+        idades[0] = 15;
+        idades[1] = 20;
+        idades[2] = 25;
+        System.out.println(idades[2]);
+    }
+}
